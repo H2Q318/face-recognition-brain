450f83f2baafc4c29439b9f13676a96bef4b2b99
Hung Hoang <hqhung201@gmail.com>
2021-09-29 20:36:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import "./App.css";
+import "tachyons";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
