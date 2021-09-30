3102433bc92c2f8cd394ee9d120b114034064a63
Hung Hoang <hqhung201@gmail.com>
2021-09-30 10:31:51 +0700

    Particle react js add image link form add face recognition install clarifai

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
-import React from "react";
-import Navigation from "./components/Navigation/Navigation";
-import Logo from "./components/Logo/Logo";
-import "./App.css";
-import "tachyons";
+import React, { Component } from 'react'
+import Particles from 'react-particles-js'
+import Clarifai from 'clarifai'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import './App.css'
+import 'tachyons'
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-    </div>
-  );
+const app = new Clarifai.App({
+    apiKey: 'b74ecc851c7448979657b880ee74f455',
+})
+
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 90,
+            density: {
+                enable: true,
+                value_area: 1000,
+            },
+        },
+    },
 }
 
-export default App;
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+            imageUrl: '',
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({ input: event.target.value })
+    }
+
+    onButtonSubmit = () => {
+        this.setState({ imageUrl: this.state.input })
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+            function (response) {
+                console.log(
+                    response.outputs[0].data.regions[0].region_info.bounding_box
+                )
+            },
+            function (err) {}
+        )
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <Particles className='particles' params={particlesOptions} />
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition imageUrl={this.state.imageUrl} />
+            </div>
+        )
+    }
+}
+
+export default App
